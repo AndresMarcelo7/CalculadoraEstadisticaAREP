ad0ab1beef1ca72a3e96fa81781fe908d3a869c0
Andres Marcelo <felipemarcelo156@gmail.com>
2020-08-06 23:11:27 -0500

    Adding Calculator class and Input
    Class calculator created with SD and mean functionalities.
    Also,  main class was edited, I put a Scanner in order to read the data from a txt file

--- src/main/java/edu/escuelaing/arep/CalculadoraEstadistica/App.java
+++ src/main/java/edu/escuelaing/arep/CalculadoraEstadistica/App.java
@@ -1,28 +1,52 @@
 package edu.escuelaing.arep.CalculadoraEstadistica;
-
+import edu.escuelaing.arep.CalculadoraEstadistica.Calculator.Calculator;
 import edu.escuelaing.arep.CalculadoraEstadistica.DataStructure.LinkedList;
 
-import java.math.BigDecimal;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
- * Main Application, Reader 
- *
+ * Main Application, Reader
  */
 public class App
 {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        LinkedList<BigDecimal> linkedList = new LinkedList<BigDecimal>();
-        linkedList.add(new BigDecimal("0.33"));
-        linkedList.add(new BigDecimal("0.33"));
-        linkedList.add(new BigDecimal("0.35"));
-        linkedList.add(new BigDecimal("0.36"));
-
-        for(BigDecimal i: linkedList){
-            System.out.println(linkedList.remove());
-
+        try {
+            File myObj = new File("Data.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                CalculateData(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
+
+    }
+
+    /**
+     * Auxiliary method that Calculates mean and SD line by line.
+     * @param data The string of Double numbers separated by "," in the file Data.txt
+     */
+
+    private static void CalculateData(String data) {
+        LinkedList<Double> ll = new LinkedList<>();
+        Calculator cal = new Calculator();
+        String[] parts = data.split(",");
+        for (String i: parts){
+            double d = Double.parseDouble(i);
+            ll.add(d);
+        }
+        System.out.println("Mean: " + cal.mean(ll) + " SD: " + cal.standardDeviation(ll));
 
     }
 }
